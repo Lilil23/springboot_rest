0c8b12558ac05f71a23c99eed4ac87124fb38761
Lilil23 <lejla.idrisova@list.ru>
2023-05-11 17:35:01 +0300

    First try

---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,42 @@
+package web.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import java.util.List;
+
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("SELECT u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void removeUser(int id) {
+        entityManager.remove(getUserById(id));
+    }
+
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,59 @@
+package web.model;
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "User")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private int age;
+
+    public User() {}
+
+    public User(int id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void serSurname(String lastname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
